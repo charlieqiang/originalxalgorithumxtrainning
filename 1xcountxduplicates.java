278d0fdabc2ba5bf8af1ce93df66b5c23348553d
charlie <327371999@qq.com>
2021-03-23 10:53:03 +0800

    2021-0323-init-clq

---
+++ 1xcountxduplicates.java
@@ -0,0 +1,17 @@
+// Description
+// Given an array of integers, your task is to find the duplicate numbers in the array.
+// Your program should return all duplicate numbers and order them in order of
+// where they started repeating.
+// For example, the array [5,1,2,2,1,1] has two duplicate digits,1 and 2.
+// The number 1 repeats at index 4, and the number 2 repeats at index 3,
+// so your program should return [2, 1], because the 2 repeats before the 1 repeats.
+
+public class Solution {
+  /**
+   * @param nums: a integer array
+   * @return: return an integer denoting the number of non-unique(duplicate) values
+   */
+  public List<Integer> countduplicates(List<Integer> nums) {
+      // write your code here
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # originalxalgorithumxtrainning
+
+```java
+// tips: the x is the space in this title.
+```
+
+This repository is the record for training algorithum. And I hope I will keep! Peace.
